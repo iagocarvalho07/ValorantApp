25629273ec38c6bf76cf543c0820221addb0724a
iagocarvalho07 <games.carvalho@hotmail.com>
2023-07-13 20:45:07 -0300

    Update DetalhesScreen.kt

--- app/src/main/java/com/iagocarvalho/valorantapp/ui/Screen/DetalhesScreen.kt
+++ app/src/main/java/com/iagocarvalho/valorantapp/ui/Screen/DetalhesScreen.kt
@@ -1,24 +1,20 @@
 package com.iagocarvalho.valorantapp.ui.Screen
 
-import android.view.Display
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavController
 
 @Composable
 fun DetalhesScreen(
     navController: NavController,
-    viewModel: DetalhesScreenViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+
 
 ) {
-    val valorantDetalhesCards = viewModel.valorantAgents.observeAsState()
 
-    LaunchedEffect(Unit) {
-        viewModel.fetDetalhesValorantAgents()
-    }
+
+
     Text(text = "DetalheScreen")
 
 
